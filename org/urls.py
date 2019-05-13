5de97b3f7907f393916fa3a9254bd0147839c437
rookie6 <18339175563@163.com>
2019-05-13 18:40:02 +0800

    school和specialty接收参数

--- org/urls.py
+++ org/urls.py
@@ -5,15 +5,14 @@ from org.views import *
 urlpatterns = [
     path('', index, name='index'),
     path('school/', school, name='school'),
-    # path('school/', ajax_get, name='school'),
+    path('school/<scid>/', school_detail, name='school_detail'),
     path('specialty/', specialty, name='specialty'),
-    path('specialty_school/', specialty_school, name='specialty_school'),
+    path('specialty/<spid>/', specialty_detail, name='specialty_detail'),
+    path('specialty/school/', specialty_school, name='specialty_school'),
     path('lineprovince/', lineprovince, name='lineprovince'),
     path('lineschool/', lineschool, name='lineschool'),
     path('linespecialty/', linespecialty, name='linespecialty'),
     path('forecast/', forecast, name='forecast'),
-    path('school_detail/', school_detail, name='school_detail'),
-    path('specialty_detail/', specialty_detail, name='specialty_detail'),
     path('school_compare/', school_compare, name='school_compare'),
 ]
 
